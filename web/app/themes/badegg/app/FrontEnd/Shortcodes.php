890ad8eff36235f3944bd45f2717a66b8f868d58
Steve Ross <git.commits@mailbox.org>
2026-02-16 18:14:00 +0000

    shortcode: angle-cta

--- web/app/themes/badegg/app/FrontEnd/Shortcodes.php
+++ web/app/themes/badegg/app/FrontEnd/Shortcodes.php
@@ -12,20 +12,33 @@ class Shortcodes
     public function arrow_cta($atts, $content = null)
     {
         $atts = shortcode_atts([
-            'direction' => 'right-down',
+            'direction' => 'right',
+            'angle' => 0,
             'text' => '',
             'colour' => 'tertiary-dark',
             'size' => 2,
         ], $atts, 'arrow_cta' );
 
         $html = '';
+        $innerHTML = '';
+
+        $angle = (int)str_replace('deg', '', $atts['angle']);
+        if(($angle > 0) && $atts['direction'] == 'left') $angle = $angle * -1;
+
+        $arrow = "<i class=\"arrow-cta-arrow\" style=\"rotate: {$angle}deg\"></i>";
+        $heading = "<span>" . ( $content ?: $atts['text']) . "</span>";
+
+        if($atts['direction'] == 'left') $innerHTML .= $arrow;
+        $innerHTML .= $heading;
+        if($atts['direction'] == 'right') $innerHTML .= $arrow;
+
 
         if($atts['text'] || $content):
             ob_start(); ?>
 
-                <div class="arrow-cta">
-                    <h<?= $atts['size'] ?> class="<?= $atts['colour'] ?>">
-                        <span><?= $content ?: $atts['text'] ?></span> <i class="arrow-cta-arrow arrow-cta-<?= $atts['direction'] ?>">&nbsp;</i>
+                <div class="arrow-cta <?= $atts['direction'] ?> section-m">
+                    <h<?= $atts['size'] ?> class="arrow-cta-heading <?= $atts['colour'] ?>">
+                        <?= $innerHTML ?>
                     </h<?= $atts['size'] ?>>
                 </div>
 
