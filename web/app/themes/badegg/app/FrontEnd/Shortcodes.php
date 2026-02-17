04008b1e88eb219c25e7a20c220ae04e324f0d54
Steve Ross <git.commits@mailbox.org>
2026-02-17 14:39:38 +0000

    button shortcode

--- web/app/themes/badegg/app/FrontEnd/Shortcodes.php
+++ web/app/themes/badegg/app/FrontEnd/Shortcodes.php
@@ -6,7 +6,48 @@ class Shortcodes
 {
     public function __construct()
     {
+        add_shortcode('button', [$this, 'button']);
         add_shortcode('arrow_cta', [$this, 'arrow_cta']);
+    }
+
+    public function button($atts)
+    {
+        $atts = shortcode_atts([
+            'href' => '#',
+            'text' => __('click here', 'badegg'),
+            'colour' => 'tertiary',
+            'class' => '',
+            'size' => '',
+            'target' => '',
+            'rel' => '',
+        ], $atts );
+
+        $classes = [
+            'btn',
+            $atts['colour'],
+        ];
+
+        if($atts['class']) $classes[] = $atts['class'];
+        if($atts['size']) $classes[] = $atts['size'];
+
+        $attributes = [
+            'href' => $atts['href'],
+            'class' => implode(' ', $classes),
+        ];
+
+        if($atts['target']) $attributes['target'] = $atts['target'];
+        if($atts['rel']) $attributes['rel'] = $atts['rel'];
+
+        $html = '';
+
+            ob_start(); ?>
+
+                <a<?php foreach($attributes as $att => $value) { echo ' ' . $att . '="' . $value . '"'; } ?>><?= $atts['text'] ?></a>
+
+            <?php $html = ob_get_clean();
+
+
+        return $html;
     }
 
     public function arrow_cta($atts, $content = null)
@@ -17,7 +58,7 @@ class Shortcodes
             'text' => '',
             'colour' => 'tertiary-dark',
             'size' => 2,
-        ], $atts, 'arrow_cta' );
+        ], $atts );
 
         $html = '';
         $innerHTML = '';
