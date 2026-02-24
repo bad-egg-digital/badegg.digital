0adde2f97cfcec8154dfbf87d61190aeb2f89870
Steve Ross <git.commits@mailbox.org>
2026-02-24 17:08:24 +0000

    lazy svg header logo fallback

--- web/app/themes/badegg/resources/views/sections/header/header-brand.blade.php
+++ web/app/themes/badegg/resources/views/sections/header/header-brand.blade.php
@@ -1,3 +1,3 @@
-<div class="logo-bad-egg-digital" data-svg="{{ Vite::asset('resources/images/logo-bad-egg-digital.svg') }}">
-  {!! $logo !!}
+<div class="logo-bad-egg-digital lazy" data-svg="{{ Vite::asset('resources/images/logo-bad-egg-digital.svg') }}">
+  <img src="{{ Vite::asset('resources/images/logo-bad-egg-digital-colour.svg') }}" alt="Bad Egg Digital Logo" width="160" height="133" />
 </div>
