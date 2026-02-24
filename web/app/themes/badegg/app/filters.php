3200051203d7e0393ad4b89fbae070a5d1e7915b
Steve Ross <git.commits@mailbox.org>
2026-02-24 11:29:45 +0000

    remove ID from body and post class functions

--- web/app/themes/badegg/app/filters.php
+++ web/app/themes/badegg/app/filters.php
@@ -17,3 +17,41 @@ add_filter('excerpt_more', function () {
 
 add_filter('wpcf7_autop_or_not', '__return_false');
 add_filter('wpcf7_load_css', '__return_false');
+
+add_filter('body_class', __NAMESPACE__ . '\\remove_bodyClasses', 10, 3);
+function remove_bodyClasses($wp_classes, $extra_classes) {
+    $post = get_post();
+    $post_id = get_the_ID();
+    $post_type = get_post_type($post_id);
+    $slug = @$post->post_name;
+
+    $wp_classes = array_diff($wp_classes, [
+        'postid-' . $post_id,
+        'page-id-' . $post_id,
+        $post_type,
+        $slug,
+    ]);
+
+    $better_classes = [];
+
+    if(is_singular()) {
+        $better_classes[] = 'wp-singular-' . $post_type;
+        $better_classes[] = 'slug-' . $slug;
+    }
+
+    return array_merge( $wp_classes, $better_classes, (array) $extra_classes);
+}
+
+
+add_filter('post_class', __NAMESPACE__ . '\\remove_postClasses', 10, 3);
+function remove_postClasses($classes, $class, $post_id) {
+    $classes = array_diff($classes, [
+        'hentry',
+        'post-' . $post_id,
+        'status-' . get_post_status($post_id),
+        get_post_type($post_id),
+        // 'type-' . get_post_type($post_id),
+    ]);
+
+    return $classes;
+}
