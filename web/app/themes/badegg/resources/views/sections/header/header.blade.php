78648b4d9dc436a550858007c4424df89e035acc
Steve Ross <git.commits@mailbox.org>
2026-02-28 15:27:10 +0000

    add class to header if first block has contrast enabled

--- web/app/themes/badegg/resources/views/sections/header/header.blade.php
+++ web/app/themes/badegg/resources/views/sections/header/header.blade.php
@@ -1,4 +1,4 @@
-<header class="menu-fixed inner inner-small inner-zero-x bg-white">
+<header class="menu-fixed inner inner-small inner-zero-x bg-white @if($firstBlockContrast) first-block-knockout @endif">
   <div class="container container-large">
     @if (has_nav_menu('primary_navigation'))
       <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
