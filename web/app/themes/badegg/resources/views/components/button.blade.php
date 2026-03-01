24436b4943b3c46bd2f65770b82e7dccab4d4439
Steve Ross <git.commits@mailbox.org>
2026-03-01 17:02:28 +0000

    add tint support to button

--- web/app/themes/badegg/resources/views/components/button.blade.php
+++ web/app/themes/badegg/resources/views/components/button.blade.php
@@ -1,5 +1,5 @@
 @if(@$link)
-  <a href="{{ @$link['url'] }}" target="{{ @$link['target'] ?: '_self' }}" class="btn {{ @$colour ?: 'primary' }} {{ $style ?: 'solid' }} {{ @$class }}">
+  <a href="{{ @$link['url'] }}" target="{{ @$link['target'] ?: '_self' }}" class="btn {{ $CssClasses->ColourTintClass(@$colour, @$tint) }} {{ @$style ?: 'solid' }} {{ @$class }}">
     <span>{{ @$link['title'] }}</span>
   </a>
 @endif
