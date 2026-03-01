2ff84f453b4e81553dc3ed765cf599388c30feb1
Steve Ross <git.commits@mailbox.org>
2026-03-01 08:58:06 +0000

    apply scrolled class when starting scrolled

--- web/app/themes/badegg/resources/views/sections/header/header.js
+++ web/app/themes/badegg/resources/views/sections/header/header.js
@@ -1,17 +1,24 @@
 export default function Header() {
-  const body = document.querySelector("body");
+  const header = document.querySelector(".menu-fixed");
+
+  toggleScrolledClass(header);
 
   document.addEventListener("scroll", () => {
-    const scrolled = document.scrollingElement.scrollTop;
-    const position = body.offsetTop;
-    const header = document.querySelector(".menu-fixed");
-
-    if(!header) return;
-
-    if (scrolled > position + (header.offsetHeight / 2)) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-    }
+    toggleScrolledClass(header);
   });
+}
+
+function toggleScrolledClass(el)
+{
+  if(!el) return;
+
+  const body = document.querySelector("body");
+  const scrolled = document.scrollingElement.scrollTop;
+  const position = body.offsetTop;
+
+  if (scrolled > position + (el.offsetHeight / 2)) {
+    el.classList.add("scrolled");
+  } else {
+    el.classList.remove("scrolled");
+  }
 }
